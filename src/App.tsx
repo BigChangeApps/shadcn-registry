862a852e1943a00e88cb6abd1aa0b3f67d22f1db
Craig Bryan <cbryansoftware@gmail.com>
2025-03-27 12:11:49 +0000

    Save tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div className="max-w-xl mx-auto my-12 rounded-lg border border-gray-200 p-8">
       <h1 className="text-2xl font-bold text-gray-900 mb-4">Custom Registry</h1>
-      <p>Add your custom components here to preview them.</p>
-
+      <p className="mb-4">Add your custom components here to preview them.</p>
+      <Button>This is a test button</Button>
     </div>
   );
 }
